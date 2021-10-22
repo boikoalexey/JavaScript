3bc04d1bca48064f02ff3a4611cf2d3d523bac0a
boikoalexey <91904042+boikoalexey@users.noreply.github.com>
2021-10-22 18:44:53 +0300

    JS

---
+++ hello.js
@@ -0,0 +1,9 @@
+        let count = 0
+        function doingSomethingBoring () {
+        count++;
+        if (count > 10) {
+        alert("Yaaaaaawwwnnnnnnnnn!");
+        } else {
+        alert("This one time, at band camp.... ");
+        }
+        }
